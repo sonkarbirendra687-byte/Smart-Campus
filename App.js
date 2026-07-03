89240475a82b965e13478c8b4aeca5f81a786e01
sonkarbirendra687-byte <sonkarbirendra687@gmail.com>
2026-07-03 16:51:35 +0530

    Added App.js file.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Smart Campus!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 20, color: 'blue' }
+});
